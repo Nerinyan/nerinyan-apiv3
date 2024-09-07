a55e20d2e6d94e3f8b5eddd872f736121bfbb102
thftgr <thftgr@gmail.com>
2024-09-07 09:10:25 +0900

    init

---
+++ src/main/kotlin/moe/nerinyan/nerinyanapiv3/NerinyanApiv3Application.kt
@@ -0,0 +1,11 @@
+package moe.nerinyan.nerinyanapiv3
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NerinyanApiv3Application
+
+fun main(args: Array<String>) {
+    runApplication<NerinyanApiv3Application>(*args)
+}
---
+++ src/test/kotlin/moe/nerinyan/nerinyanapiv3/NerinyanApiv3ApplicationTests.kt
@@ -0,0 +1,13 @@
+package moe.nerinyan.nerinyanapiv3
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NerinyanApiv3ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
